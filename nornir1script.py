408aa94f3f801f6306943f4ae50950d6c183035f
navyny <navyny@gmail.com>
2021-05-29 23:13:49 -0700

    Update nornir1script.py

--- nornir1script.py
+++ nornir1script.py
@@ -4,6 +4,22 @@ from nornir_utils.plugins.functions import print_result                         
 
 
                                                                                  #https://github.com/ktbyers/nornir_netmiko/tree/master/nornir_netmiko/tasks
-  nr123 = InitNornir(config.yml)
+  nr123 = InitNornir("config.yml")
   output123 = nr123.run(netmiko_Send_command, command_string = "show ip int br")
   print_result(output123)
+
+
+  ########*******################
+  
+  from nornir_napalm.plugins.tasks import napalm_get,napalm_cli
+  
+  output123 = nr123.run(napalm_get, getters=["get_factts"])
+  output123 = nr123.run(napalm_cli, commands=["show clock", "show ip int br", "show cdp nei"])
+  print_result
+  print_result(output123)
+  
+  
+  ====> FOR PYNTC & NAPALM output will be in dictionary format
+          in PYNTC it was converting output to strings 
+  
+  ===> FOR NETMIKO output will be cleaner as we could use  JSON
